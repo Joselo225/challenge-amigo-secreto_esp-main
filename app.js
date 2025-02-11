48dc3fb98428d7919203e4898fde273e66b8e5dd
JoseMorales <jose.morales78@inacapmail.cl>
2025-02-11 20:59:42 -0300

    Se agregaron las funciones de agregar amigos a un array y mostrar la lista de amigos en pantalla a medida se van agregando a la lista

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+//Array que guardará los nombres de las personas agregadas
+
+let listaAmigos = [];
+
+//Funcion que agregue un amigo a la lista
+function agregarAmigo() {
+    //Crea la variable de amigo que se agrega y obtiene el valor del input
+    let amigoAgregado;
+    amigoAgregado = document.getElementById("amigo").value;
+    //console.log(amigoAgregado);
+    
+    //Valida que el input no esté vacío
+    if (amigoAgregado == "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+        //Valida que el input no tenga nombres repetidos
+    }else if (listaAmigos.includes(amigoAgregado)) {
+        alert("El nombre ya fue ingresado");
+        return
+        //Agregar el nombre a la lista
+    }else{listaAmigos.push(amigoAgregado);
+    //console.log(listaAmigos);
+    //Vuelve a llamar al imput para dejar el campo en blanco
+    document.getElementById("amigo").value = "";
+
+    }
+    mostrarAmigos(listaAmigos);
+}
+function mostrarAmigos(listaAmigos) {
+    //Crea la variable que guardará el texto a mostrar
+    let textoAmigos = document.getElementById("listaAmigos");
+    //Deja en blanco el texto
+    textoAmigos.innerHTML = "";
+    
+    //Recorre el array de amigos y los agrega al texto
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.textContent = listaAmigos[i];
+        textoAmigos.appendChild(itemLista);
+        console.log(listaAmigos);
+    }
+    //Muestra el texto en el div
+}
+
